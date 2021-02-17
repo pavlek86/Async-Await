54fae3e240cb30905c725ee5be3276bcffe0538b
pavlek86 <pavlek86@gmail.com>
2021-02-17 02:19:52 +0100

    Implemented synchronous execution

--- Async-Await/MainWindow.xaml.cs
+++ Async-Await/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +25,66 @@ namespace Async_Await
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void executeSync_Click(object sender, RoutedEventArgs e)
+        {
+            var watch = Stopwatch.StartNew();
+            
+            RunDownloadSync();
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            resultWindow.Text += $"Total execution time: { elapsedMs }";
+            
+        }
+
+        private void executeAsync_Click(object sender, RoutedEventArgs e)
+        {
+            throw new Exception("Not implemented");
+        }
+
+        private List<string> GetWebsites()
+        {
+            List<string> output = new List<string>();
+
+            resultWindow.Text = "";
+
+            output.Add("https://www.google.com");
+            output.Add("https://www.microsoft.com");
+            output.Add("https://www.ubuntu.com");
+            output.Add("https://www.github.com");
+            output.Add("https://stackoverflow.com");
+
+            return output;
+        }
+
+        private void RunDownloadSync()
+        {
+            List<string> websites = GetWebsites();
+
+            foreach (string site in websites)
+            {
+                WebsiteDataModel results = DownloadWebsite(site);
+                ReportWebsiteInfo(results);
+            }
+        }
+
+        private WebsiteDataModel DownloadWebsite(string websiteURL)
+        {
+            WebsiteDataModel output = new WebsiteDataModel();
+            WebClient client = new WebClient();
+            
+            output.WebsiteUrl = websiteURL;
+            output.WebsiteData = client.DownloadString(websiteURL);
+
+            return output;
+        }
+
+        private void ReportWebsiteInfo(WebsiteDataModel data)
+        {
+            resultWindow.Text += $"{ data.WebsiteUrl } downloaded: { data.WebsiteData.Length } characters long. { Environment.NewLine }";
         }
     }
 }
